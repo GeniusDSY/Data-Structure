cd052ccf672139cc270e5ee7044e2b4e6f0b146f
Genius_DSY <genius_dsy@foxmail.com>
2019-09-07 17:12:09 +0800

    :art:添加链表队列的对比测试

--- Queue/src/PerformanceTest.java
+++ Queue/src/PerformanceTest.java
@@ -21,7 +21,7 @@ public class PerformanceTest {
     }
 
     public static void main(String[] args) {
-        int opCount = 10000000;
+        int opCount = 100000;
         ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
         double time1 = testQueue(arrayQueue,opCount);
         System.out.println("ArrayQueue,time：" + time1 + "s");
@@ -29,5 +29,9 @@ public class PerformanceTest {
         LoopQueue<Integer> loopQueue = new LoopQueue<>();
         double time2 = testQueue(loopQueue,opCount);
         System.out.println("LoopQueue,time：" + time2 + "s");
+
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        double time3 = testQueue(linkedListQueue,opCount);
+        System.out.println("LinkedListQueue,time：" + time3 + "s");
     }
 }
